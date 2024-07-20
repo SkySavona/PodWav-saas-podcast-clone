066cd99f7f656e8f2d53398b92c35ce192037103
Sky <142328892+SkySavona@users.noreply.github.com>
2024-07-20 11:21:03 -0700

    trending podcast funciton update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,33 @@
 "use client";
 import React from "react";
-// import { Button } from "@/components/ui/button";
 import PodcastCard from "@/components/PodcastCard";
-import { podcastData } from "@/constants";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import LoaderSpinner from '@/components/LoaderSpinner';
+
 
 const Home = () => {
-  // const tasks = useQuery(api.tasks.get);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+
+  if(!trendingPodcasts) return <LoaderSpinner />
+
   return (
     <div className="mt-0 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
         {/* <Button className="text-white-1 bg-blue-3">Button</Button> */}
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
-        </div>
 
         <div className="podcast_grid">
-          {podcastData.map(({ id, title, description, imgURL }) => (
-            <PodcastCard
-              key={id}
-              imgURL={imgURL}
-              title={title}
-              description={description}
-              podcastId={id}
-            />
-          ))}
+          {trendingPodcasts?.map(
+            ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard
+                key={_id}
+                imgURL={imageUrl as string}
+                title={podcastTitle}
+                description={podcastDescription} podcastId={0}                // podcastId={_id}
+              />
+            )
+          )}
         </div>
       </section>
     </div>
