425a7efd32733c49bfd34c4dd6600250c555dc9d
Sky <142328892+SkySavona@users.noreply.github.com>
2024-07-21 16:52:05 -0700

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,21 @@ const publicRoutes = ["/", "/sign-up(.*)"];
 const isPublicRoute = createRouteMatcher(publicRoutes);
 
 export default clerkMiddleware((auth, req, evt) => {
-  if (!isPublicRoute(req)) {
-    const { userId } = auth();
-    if (!userId) {
-      const signInUrl = new URL('/sign-in', req.url);
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
+  const { userId } = auth();
+
+  // Allow access to the sign-in page and public routes without redirection
+  if (req.nextUrl.pathname === '/sign-in' || isPublicRoute(req)) {
+    return NextResponse.next();
   }
+
+  // Redirect to sign-in only if user is not authenticated and trying to access a protected route
+  if (!userId) {
+    const signInUrl = new URL('/sign-in', req.url);
+    signInUrl.searchParams.set('redirect_url', req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  // For all other cases, allow the request to proceed
   return NextResponse.next();
 });
 
