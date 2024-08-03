b78237b95302121638eac69b29708e90a1985814
Skyllar Savona <Skyllar Savona@Skys-MacBook-Pro.local>
2024-08-03 09:36:01 -0700

    login with email an password configured

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const publicRoutes = ["/", "/sign-up(.*)"];
+
+const publicRoutes = ["/", "/sign-in(.*)", "/sign-up(.*)", "/api/(.*)"];
 const isPublicRoute = createRouteMatcher(publicRoutes);
 
 export default clerkMiddleware((auth, req, evt) => {
   const { userId } = auth();
 
-  // Allow access to the sign-in page and public routes without redirection
-  if (req.nextUrl.pathname === '/sign-in' || isPublicRoute(req)) {
+  // Allow access to public routes and Clerk-related routes without redirection
+  if (isPublicRoute(req) || req.nextUrl.pathname.startsWith("/.clerk")) {
     return NextResponse.next();
   }
 
   // Redirect to sign-in only if user is not authenticated and trying to access a protected route
   if (!userId) {
-    const signInUrl = new URL('/sign-in', req.url);
-    signInUrl.searchParams.set('redirect_url', req.url);
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
     return NextResponse.redirect(signInUrl);
   }
 
